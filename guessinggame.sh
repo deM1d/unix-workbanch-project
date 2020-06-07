99352ec2420f15511336a20630dc39c73a44ae2e
root <root@milliondollarproject.(none)>
2020-06-07 13:11:08 +0000

    fixed script

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,10 +1,19 @@
-function read_user_answer {
+function user_input {
   echo "How many files there are in current directory?"
-  read user_answer
+  read user_answer 
+}
+
+function read_user_answer {
+  user_input
+  while ! [[ $user_answer =~ ^[0-9]+$ ]]
+  do
+    echo "$user_answer is not a number. Write coorect number."
+    user_input
+  done
 }
 
 echo "In this simple game you must guess how many files in current directory."
-correct_answer=$(ls | wc -l)
+correct_answer=$(find . -maxdepth 1 -type f | wc -l)
 read_user_answer
 
 while [[ $user_answer -ne $correct_answer ]]
